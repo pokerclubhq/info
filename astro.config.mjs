52ae2e0f56dee28e7643d7ba25e36e3c529452a0
Ron Booth <ron@ronbooth.com>
2026-01-29 12:29:34 -0700

    update to config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pokerclubhq.github.io',
+	base: '/info',
 	integrations: [
 		starlight({
 			title: 'My Docs',
